d1a08789e6c4ebf2b42b31812aa0eb03d0297d77
Jakub Kucera <jakub.kucerja@gmail.com>
2023-09-19 16:50:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ def _requires_from_file(filename):
 
 setup(
     name="isc-feature-extractor",
-    version="1.0.1",
-    url="https://github.com/lyakaap/ISC21-Descriptor-Track-1st",
+    version="1.0.2",
+    url="https://github.com/jakub-kucera/ISC21-Descriptor-Track-1st",
     author="lyakaap",
     packages=find_packages(exclude=["exp", "scripts"]),
     python_requires=">=3.7",
